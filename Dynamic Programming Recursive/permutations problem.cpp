6131a411c3a4b883c18bf6605ce4741787328bdb
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-05-17 21:40:44 +0530

    added permutations problem in DP

---
+++ Dynamic Programming Recursive/permutations problem.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+static int dp[1001][801];
+// #define mod 1000000007;
+
+
+int solve(int n, int r){
+        if(r>n)
+        return 0;
+        if(r==0||n==r)
+        return dp[n][r] = 1;
+        
+        
+        if(dp[n][r]!=-1)
+        return dp[n][r]%1000000007;
+        
+        return dp[n][r] = (solve(n-1, r-1)%1000000007 + solve(n-1,r)%1000000007)%1000000007;
+        
+
+    
+}
+
+
+
+
+        int nCr(int n, int r){
+            
+            memset(dp, -1, sizeof(dp));
+            
+            return solve(n, r);
+            
+            }
+
+    
