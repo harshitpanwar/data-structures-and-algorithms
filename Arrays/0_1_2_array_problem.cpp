9f301af80a3961b81b75586fd012994ae1485ab8
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-08-18 05:45:38 +0530

    0, 1, and 2 array

---
+++ Arrays/0_1_2_array_problem.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+//initialize your vector here
+//call the sortcolors function here
+
+return 0;
+
+}
+
+ void sortColors(vector<int>& nums) {
+       int lo = 0; 
+        int hi = nums.size() - 1; 
+        int mid = 0; 
+
+        while (mid <= hi) { 
+            switch (nums[mid]) { 
+
+            // If the element is 0 
+            case 0: 
+                swap(nums[lo++], nums[mid++]); 
+                break; 
+
+            // If the element is 1 . 
+            case 1: 
+                mid++; 
+                break; 
+
+            // If the element is 2 
+            case 2: 
+                swap(nums[mid], nums[hi--]); 
+                break; 
+            }
+        }
+    }
