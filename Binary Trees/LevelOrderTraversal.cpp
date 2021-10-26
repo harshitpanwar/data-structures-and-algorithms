0959470532f64e34eb3a5c7ade210390300d461c
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-10-26 23:00:01 +0530

    Level Order Traversal of a Binary Tree completed

--- Binary Trees/LevelOrderTraversal.cpp
+++ Binary Trees/LevelOrderTraversal.cpp
@@ -2,7 +2,46 @@
 using namespace std;
 
 int main(){
-
+//driver code goes here
 
 }
 
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+
+
+class Solution
+{
+   public:
+   //Function to return the level order traversal of a tree.
+   vector<int> levelOrder(Node* node)
+   {
+     //Your code here
+     vector<int> ans;
+     queue<Node*> q;
+     q.push(node);
+     if(node==NULL)
+     return ans;
+     while(!q.empty()){
+         Node* temp=q.front();
+         q.pop();
+         ans.push_back(temp->data);
+         if(temp->left!=NULL)
+         q.push(temp->left);
+         if(temp->right!=NULL)
+         q.push(temp->right);
+     }
+     return ans;
+   }
+};
+ 
