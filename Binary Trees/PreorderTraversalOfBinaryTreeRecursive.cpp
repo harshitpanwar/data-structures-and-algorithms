8564c0aeb88a098870c54731f75b068fadd40e5f
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-10-30 16:26:17 +0530

    Preorder Traversal of a Binary Tree recursive

---
+++ Binary Trees/PreorderTraversalOfBinaryTreeRecursive.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+ 
+struct Node
+{
+    int data;
+    Node *left, *right;
+ 
+    Node(int data)
+    {
+        this->data = data;
+        this->left = this->right = nullptr;
+    }
+};
+ 
+void preorder(Node* root)
+{
+    if (root == nullptr) {
+        return;
+    }
+ 
+    cout << root->data << " ";
+ 
+    preorder(root->left);
+ 
+    preorder(root->right);
+}
+ 
+ 
+int main()
+{
+    //driver code goes here
+ 
+    return 0;
+}
+
