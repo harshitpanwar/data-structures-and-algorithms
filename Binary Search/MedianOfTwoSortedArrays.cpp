36c922b2d046ce9e95b06ad8641b0f6568ab671a
Harshit Panwar <harshitpanwar234567@gmail.com>
2021-12-16 21:38:45 +0530

    median of two sorted array done

--- Binary Search/MedianOfTwoSortedArrays.cpp
+++ Binary Search/MedianOfTwoSortedArrays.cpp
@@ -12,14 +12,12 @@ public:
         while(low <= high) {
             int cut1 = (low+high) >> 1;
             int cut2 = (n1 + n2 + 1) / 2 - cut1; 
-            
         
             int left1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
             int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1]; 
             
             int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
             int right2 = cut2 == n2 ? INT_MAX : nums2[cut2]; 
-            
             
             if(left1 <= right2 && left2 <= right1) {
                 if( (n1 + n2) % 2 == 0 ) 
