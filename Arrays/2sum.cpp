f40e7254f6a7abc770ab26f9b2cca75933c6e4bd
Harshit Panwar <harshitpanwar234567@gmail.com>
2022-04-08 23:39:31 +0530

    2 sum problem done

---
+++ Arrays/2sum.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //driver code here
+    
+    return 0;
+}
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans; 
+        unordered_map<int, int> mpp; 
+        for(int i = 0;i<nums.size();i++) {
+            if(mpp.find(target - nums[i]) != mpp.end()) {
+                ans.push_back(mpp[target-nums[i]]); 
+                ans.push_back(i); 
+                return ans; 
+            }
+            mpp[nums[i]] = i; 
+        }
+        return ans; 
+    }
+};
