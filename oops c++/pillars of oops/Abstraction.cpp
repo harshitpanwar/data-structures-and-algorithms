98f7c41d352b6b89bc140d2241c1a0b4823cea99
Harshit Panwar <harshitpanwar234567@gmail.com>
2022-12-19 18:30:02 +0530

    abstraction new material added

--- oops c++/pillars of oops/Abstraction.cpp
+++ oops c++/pillars of oops/Abstraction.cpp
@@ -7,6 +7,20 @@ using namespace std;
 
 int main(){
 
+    // pointers
+    int a = 20;
+    int *p = &a;
+    int b = *p; 
+
+    cout<<"a "<<a<<" *p "<<*p<<" p "<<p<<" b "<<b<<endl;
+
+
+    //pointer to pointer
+
+    int **pp = &p;
+
+    cout<<"**P "<<**pp<<" pp "<<pp<<endl;
 
     return 0;
+
 }
